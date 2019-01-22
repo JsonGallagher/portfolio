d08a94ef480ff5822030a125988022f7f916641c
Jason <jasong13@gmail.com>
2019-01-22 15:53:48 -0700

    Fixed comments

--- dist/js/main.js
+++ dist/js/main.js
@@ -18,16 +18,16 @@ function toggleMenu() {
     menuBranding.classList.add('show');
     navItems.forEach(item => item.classList.add('show'));
 
-    //Set Menu state
+    // Set Menu state
     showMenu = true;
   } else {
     menuBtn.classList.remove('close');
     menu.classList.remove('show');
     menuNav.classList.remove('show');
     menuBranding.classList.remove('show');
-    navItems.forEach(item => item.classList.remove('show'))
+    navItems.forEach(item => item.classList.remove('show'));
 
-    //Set Menu state
+    // Set Menu state
     showMenu = false;
   }
 }
